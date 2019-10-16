a5eec96a817a8a5370b22826031f9d87f55012c1
Lynn <1076448574@qq.com>
2019-10-16 17:18:56 +0800

    17.18

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler) // each request calls handler
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	log.Fatal(http.ListenAndServe("107.173.26.195:8000", nil))
 }
 
 // handler echoes the Path component of the request URL r.
